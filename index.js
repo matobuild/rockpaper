53ff482c160a1a0106be690ac1720b7e1cc1e530
Mato <Kittawat@icloud.com>
2022-10-26 23:59:30 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const output = ["Rock", "Paper", "Scissor"];
+
+function getComputerChoice() {
+    let randomResult = Math.floor(Math.random() * 3);
+    return output[randomResult]
+}
+console.log(getComputerChoice());
+
+function playRockPaperScissors (playerSelection, computerSelection) {
+    
+}
